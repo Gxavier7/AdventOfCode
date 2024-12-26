b8a01a5401bb35e08b2387df1a5382d9791cb438
Gabriel Martins Xavier <gabrim0070@gmail.com>
2024-12-25 22:10:29 -0300

    day5 Part2

--- day-05/solution.js
+++ day-05/solution.js
@@ -1,3 +1,4 @@
+const { log } = require("node:console");
 const fs = require("node:fs");
 const { release } = require("node:os");
 const path = require("node:path");
@@ -16,38 +17,94 @@ rules.forEach(rule => {
 })
 
 const verifyPageOrders = (pagesArray) => {
-  let correctlyPages = [];
+  let correctPages = [];
+  let incorrectPages = [];
 
   for (let i = 0; i < pagesArray.length; i++) {
-    const element = pagesArray[i].match(/\d+/gm);
+    const list = pagesArray[i].match(/\d+/gm);
 
-    isPageCorrectly = checkNumbers(element);
-
-    if (isPageCorrectly) correctlyPages.push(element);
+    pageResult = checkNumbers(list);    
+    
+    if ( typeof pageResult !== "string") correctPages.push(list);
+    else incorrectPages.push({
+      list,
+      number: pageResult
+    });
   }
 
-  return correctlyPages;
+  return {correctPages, incorrectPages};
 }
 
 const checkNumbers = ( list ) => {
-  isListValid = true;
-
+  let validList = true;
+  let wrongPage;
+  
   for (let i = 0; i < list.length - 1; i++) {
     for (let j = i+1; j < list.length; j++) {
       if (!rulesObject[list[i]].includes(list[j]) ) {
-        isListValid = false;
+        wrongPage = list[i];
       }
     }  
   }
 
-  return isListValid;
+  return wrongPage || validList;
 }
 
-const correctlyPages = verifyPageOrders(pageNumbers)
+const verifiedPagesArray = verifyPageOrders(pageNumbers)
 
 let total = 0;
-correctlyPages.forEach(pageList => {  
+verifiedPagesArray.correctPages.forEach(pageList => {  
   total += +pageList[Math.round(pageList.length/2)-1]
 })
+ 
+console.log(`O total da soma das medianas das listas corretas é: ${total}`)
 
-console.log(total)
+// Part 2
+
+const fixIncorrectPages = (incorrectList) => {
+  let fixedPagesArray = [];
+  const list = incorrectList.list;
+  const number = incorrectList.number;
+  const index = list.indexOf(number);
+
+  for (let i = 0; i < list.length - 1; i++) {
+    if (rulesObject[number].includes(list[i + 1]) ) {
+      list.splice(index, 1);
+      list.splice(i + 1, 0, number);
+      fixedPagesArray.push({
+        list,
+        number
+      });
+      return fixedPagesArray;
+    }
+  }
+
+  list.splice(index, 1);
+  list.splice(list.length, 0, number);
+  fixedPagesArray.push({
+    list,
+    number
+  });
+  return fixedPagesArray
+}
+
+let fixedPagesArray = verifiedPagesArray.incorrectPages.map(wrongObject => fixIncorrectPages(wrongObject));
+let incorrectPagesSum = 0;
+
+while (fixedPagesArray.length !== 0) {
+  fixedPagesArray.map( (element, index) => {
+    const object = element[0];
+    const list = object.list
+    const checkResult = checkNumbers(list);
+  
+    if (checkResult === true) {
+      incorrectPagesSum += +list[Math.round(list.length/2)-1];
+      fixedPagesArray.splice(index, 1);
+      
+    } else {
+      fixedPagesArray[index] = fixIncorrectPages({list, number: checkResult});
+    }
+  })
+}
+
+console.log(`O total da soma da mediana das listas corrigidas é: ${incorrectPagesSum}`)
